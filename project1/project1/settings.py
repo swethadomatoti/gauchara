3aa2298ffae52e9ebf6c2c52fd6d461b5a70db17
swethadomatoti <swethadomatoti@gmil.com>
2026-03-11 14:40:43 +0530

    Settings

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -77,13 +77,15 @@ WSGI_APPLICATION = 'project1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+ 
+import os
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
