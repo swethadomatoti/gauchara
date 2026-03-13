cd664a3224a7af714e95592a3f997de9b1ab6bb1
swethadomatoti <swethadomatoti@gmil.com>
2026-03-13 23:00:25 +0530

    celery

--- project1/celery.py
+++ project1/celery.py
@@ -2,7 +2,10 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')#Without this, Celery would be disconnected from your Django project.
-app = Celery('project')#Manages every tasks
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project1.settings')
+
+app = Celery('project1')
+
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks() 
+
+app.autodiscover_tasks()
